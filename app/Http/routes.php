d197c71fc09481253299e2680bd2c6e21e598ab5
Ravi Malik <ravimalik2364@gmail.com>
2015-11-29 15:16:41 +0530

    Index and auth pages added.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,15 +11,23 @@
 |
 */
 
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::group(["prefix" => "auth"], function ()
+{
+    Route::get('login', 'Auth\AuthController@getLogin');
+    Route::post('login', 'Auth\AuthController@postLogin');
+    Route::get('logout', 'Auth\AuthController@getLogout');
 
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-
-Route::resource("home", "HomeController", ["only" => ["index"]]);
-
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('register', 'Auth\AuthController@getRegister');
+    Route::post('register', 'Auth\AuthController@postRegister');
 });
+
+Route::group(["middleware" => "auth"], function ()
+{
+    Route::resource("home", "HomeController", ["only" => ["index"]]);
+
+    Route::get('/', function () {
+        return view('index');
+    });
+});
+
+
---
+++ resources/views/includes/lhs-menu.blade.php
@@ -0,0 +1,18 @@
+<ul class="sidebar-menu">
+    <li class="active">
+        <a href="#">
+            <i class="fa fa-plus"></i> <span>Add Folder</span>
+        </a>
+    </li>
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-folder"></i>
+            <span>Folder 1</span>
+            <i class="fa fa-angle-left pull-right"></i>
+        </a>
+        <ul class="treeview-menu">
+            <li><a href="pages/charts/morris.html"><i class="fa fa-angle-double-right"></i> File 1</a></li>
+            <li><a href="pages/charts/flot.html"><i class="fa fa-angle-double-right"></i> File 2</a></li>
+        </ul>
+    </li>
+</ul>
