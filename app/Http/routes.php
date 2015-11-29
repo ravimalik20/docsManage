990dad634ae1552b8609ed5b81bb7b1aa284079d
Ravi Malik <ravimalik2364@gmail.com>
2015-11-29 12:52:24 +0530

    Authentication and registration routes added without styling.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,15 @@
 |
 */
 
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::resource("home", "HomeController", ["only" => ["index"]]);
+
 Route::get('/', function () {
     return view('welcome');
 });
